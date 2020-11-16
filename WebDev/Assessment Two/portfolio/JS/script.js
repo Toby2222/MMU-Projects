0b6a65372be65278c67d756020616f99109d0fbc
Toby Abbott <a.toby.2222@gmail.com>
2020-11-16 12:20:06 +0000

    removed snapchat addon added all JS and a couple extras

--- WebDev/Assessment Two/portfolio/JS/script.js
+++ WebDev/Assessment Two/portfolio/JS/script.js
@@ -1,14 +1,17 @@
 var currenttime = new Date();
 var currenthour = currenttime.getHours();
+var offsetMins = currenttime.getTimezoneOffset();
+var offset = offsetMins/60
 
+console.log(offset);
+console.log(currenthour);
+currenthour += offset;
 console.log(currenthour);
 
-// do a conditional
+var emailmessage = "I am awake, feel free to email me";
 
-var emailmessage = "You are <strong>OK</strong> to email me now";
-
-if (currenthour < 9 || currenthour > 22) {
-  emailmessage = "I am asleep, sorry!";
+if (currenthour < 9 || currenthour >= 22) {
+  emailmessage = "I am asleep, please avoid emailing me currently";
 }
 
 var getheading = document.querySelector(".contact-me h2");
@@ -19,11 +22,46 @@ getheading.insertAdjacentHTML(
 );
 
 
-function snapcode() {
-    var codeImage = document.getElementById("codePopup");
-    codeImage.classList.toggle("appear");
+var findLocation = document.getElementById('switch');
+
+
+var button = document.createElement('button');
+button.setAttribute('class','js-make-something-happen'); 
+button.setAttribute('id','make-something-happen');
+button.textContent='Blinding Light Mode'; 
+
+
+findLocation.append(button);
+
+var listenForInteraction = document.getElementById('make-something-happen');
+
+
+function changePage(){
+  
+  document.documentElement.classList.toggle('js-modify-page');
+
+  if (button.textContent == 'Blinding Light Mode'){
+    button.textContent = 'Take me back to Dark Mode';
+    document.cookie = "false"
+  } else{
+    button.textContent = 'Blinding Light Mode';
+    document.cookie = "true;"
+  }
 }
-function snapcode2() {
-    var codeImage = document.getElementById("codePopup2");
-    codeImage.classList.toggle("appear");
-}
+
+
+listenForInteraction.addEventListener('click', changePage);
+listenForInteraction.addEventListener('keydown', changePage);
+
+
+window.onload = (event) => {
+  console.log(document.cookie);
+  if (document.cookie == "false"){
+    document.documentElement.classList.add('js-modify-page');
+    button.textContent = 'Take me back to Dark Mode';
+  } else {
+    document.documentElement.classList.remove('js-modify-page');
+    button.textContent = 'Blinding Light Mode';
+  }
+};
+
